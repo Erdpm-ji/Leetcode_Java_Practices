577d48d2d4cd7e8dfecb7b536d8bdd5926d0a81d
deepakpatidar97 <deepakpatidarmahetwada@gmail.com>
2024-11-17 13:24:02 +0530

    Find length of Last word in the string problem working find and accepted successfully

---
+++ Java_Practices/src/com/java/LengthOfLastWord.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class LengthOfLastWord {
+	public static void main(String[] args) {
+		System.out.println(lengthOfLastWord("Hello World"));
+		System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+		System.out.println(lengthOfLastWord("luffy is still joyboy"));
+		
+	}
+	 public static int lengthOfLastWord(String s) {
+	        
+		 String []words = s.split(" ");
+	        
+	     int len = words[words.length -1].length() ; 
+	        
+	        return len;
+	    }
+}
