40d747316fc46c06c589e5aabdc497746a7abfe4
deepakpatidar97 <deepakpatidarmahetwada@gmail.com>
2024-11-17 12:55:59 +0530

    Search Insert place probleam working now fine

--- Java_Practices/src/com/java/binarySearch/SearchInseartPosition.java
+++ Java_Practices/src/com/java/binarySearch/SearchInseartPosition.java
@@ -4,25 +4,20 @@ public class SearchInseartPosition {
 	public static void main(String[] args) {
 		int[] ar = { 1, 3, 5, 6 };
 		//System.out.println(searchInsert(ar, 5));
-		System.out.println(searchInsert(ar, 7));
+		System.out.println(searchInsert(ar, 2));
 	}
 
 	public static int searchInsert(int[] nums, int target) {
 
-		int low = 0, high = nums.length;
+		int low = 0, high = nums.length -1;
 		int mid = 0;
+		int ans = nums.length;
 
 		while (low <= high) {
 			mid = (low + high) / 2;
 
-			if (nums[mid] == target) {
-				return mid;
-			}
-			if(mid == high) {
-				return high+1;
-			}
-
-			if (nums[mid] > target) {
+			if (nums[mid] >= target) {
+				ans = mid;
 				high = mid - 1;
 			} else {
 				low = mid + 1;
@@ -30,7 +25,7 @@ public class SearchInseartPosition {
 
 		}
 
-		return 0;
+		return ans;
 	}
 
 }
